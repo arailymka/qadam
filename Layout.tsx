c55e414371a0fab88f4080438e404ef1c7536b95
arailymka <120112491+arailymka@users.noreply.github.com>
2026-02-25 20:18:17 +0500

    Update Layout.tsx

--- Layout.tsx
+++ Layout.tsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Language, translations, UserRole } from '../types';
+// ИСПРАВЛЕНО: удалена лишняя точка в пути
+import { Language, translations, UserRole } from './types';
 
 interface LayoutProps {
   children: React.ReactNode;
